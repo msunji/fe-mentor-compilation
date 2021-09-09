3ed2b1224ad6bd1087d4e42c198e87b25dfccbb5
Marge <marge.consunji@gmail.com>
2021-09-09 12:58:45 +0800

    Applied base global styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import GlobalStyle from "./styles/globalStyle";
+
 function App() {
   return (
     <>
+      <GlobalStyle />
       <div>Test</div>
       <div>Test</div>
     </>
--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -3,9 +3,21 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
-  // :root {
-
-  // }
+  :root {
+    --urbanist: 'Urbanist', sans-serif;
+  }
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box
+  }
+  html,
+  body {
+    overflow-x: hidden;
+    min-height: 100vh;
+    font-family: var(--urbanist);
+    font-size: 16px;
+  }
 `;
 
 export default GlobalStyle;
