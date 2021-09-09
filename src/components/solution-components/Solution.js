13a694955d8bc6e350387631f9bd80c82397c320
Marge <marge.consunji@gmail.com>
2021-09-09 15:36:26 +0800

    Map through data and show solution information

--- src/components/solution-components/Solution.js
+++ src/components/solution-components/Solution.js
@@ -8,9 +8,13 @@ const SolutionBlock = styled.div`
 export const Solution = ({ data }) => {
   console.log(data);
   return (
-    <SolutionBlock>
-      Test test
-      <p>test</p>
+    <SolutionBlock data={data}>
+      <img src={data.thumb[0].url} alt={data.challenge} />
+      <p className="challenge-title">{data.challenge}</p>
+      <div>
+        <a href={data.live}>Live Site</a>
+        <a href={data.repo}>View Code</a>
+      </div>
     </SolutionBlock>
   );
 };
