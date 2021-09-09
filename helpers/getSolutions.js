3dabf878f013444754fbd7b0a2209228238db1bf
Marge <marge.consunji@gmail.com>
2021-09-09 15:25:20 +0800

    Clean up res

--- helpers/getSolutions.js
+++ helpers/getSolutions.js
@@ -3,10 +3,14 @@ const formatRes = require("./formatRes");
 
 module.exports = async (event) => {
   try {
-    const solutions = await table.select().firstPage(); // get first 20 records
-    return formatRes(200, solutions);
+    const data = await table.select().firstPage(); // get first 20 records
+    const solns = data.map((solution) => ({
+      id: solution.id,
+      ...solution.fields,
+    }));
+    return formatRes(200, solns);
   } catch (error) {
     console.log(error);
-    return formatRes(500, {});
+    return formatRes(500, { msg: "Oops. Something went wrong." });
   }
 };
