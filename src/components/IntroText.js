d2f736b1735d61d37833fdf3f5245972e5cd6fcf
Marge <marge.consunji@gmail.com>
2021-09-10 11:40:56 +0800

    Added more to intro blurb

--- src/components/IntroText.js
+++ src/components/IntroText.js
@@ -3,20 +3,21 @@ import { Container } from "./layout/Container";
 
 const Text = styled.section`
   text-align: center;
+  background: var(--yellow);
 `;
 
 export const IntroText = () => {
   return (
     <Text>
       <Container>
+        <h1>About</h1>
         <p>
-          Here's a list of all solutions I've done for certain{" "}
-          <a href="https://www.frontendmentor.io/">Frontend Mentor</a>{" "}
-          challenges.
-        </p>
-        <p>
-          Links are provided for each project's Github Repo and Live Site. Tags
-          are added to show you what I used to solve these challenges.
+          Here you'll find a compilation of all the solutions I've completed for
+          certain <a href="https://www.frontendmentor.io/">Frontend Mentor</a>{" "}
+          challenges. Links are provided for each project's respective Github
+          repo and Live site. Tags are added to show what I I've used to solve
+          these challenges. This is a work in progress, so expect to see more
+          changes as I progress through more challenges. Cheers!
         </p>
       </Container>
     </Text>
