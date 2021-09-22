d6bbcf4b3968e31d4053b60357e4062ee4d8e99a
Marge <marge.consunji@gmail.com>
2021-09-22 15:57:49 +0800

    Added filter

--- src/components/IntroText.js
+++ src/components/IntroText.js
@@ -2,24 +2,66 @@ import styled from "styled-components";
 import { Container } from "./layout/Container";
 
 const Text = styled.section`
-  text-align: center;
-  background: var(--yellow);
+  padding: calc(2.5 * var(--padding-y)) 0;
+  background-color: var(--dark-grey);
+  color: white;
 `;
+
+const TextContainer = styled(Container)`
+  display: grid;
+  grid-template-columns: 1.5fr 1fr;
+
+  h1 {
+    padding-top: 0;
+  }
+`;
+
+const Filter = styled.div`
+  margin-left: 3rem;
+
+  .header {
+    text-transform: uppercase;
+    font-size: 0.9em;
+    letter-spacing: 0.5px;
+    color: grey;
+  }
+`;
+
+const CustomCheckBox = styled.div`
+  // input[type="checkbox"] {
+  //   display: none;
+  // }
+`;
+
+const CheckBox = ({ boxName, inputId }) => {
+  return (
+    <CustomCheckBox>
+      <input type="checkbox" id={inputId} name={inputId} />
+      <label for={boxName}>{boxName}</label>
+    </CustomCheckBox>
+  );
+};
 
 export const IntroText = () => {
   return (
     <Text>
-      <Container>
-        <h1>About</h1>
-        <p>
-          Here you'll find a compilation of all the solutions I've completed for
-          certain <a href="https://www.frontendmentor.io/">Frontend Mentor</a>{" "}
-          challenges. Links are provided for each project's respective Github
-          repo and Live site. Tags are added to show what I I've used to solve
-          these challenges. This is a work in progress, so expect to see more
-          changes as I progress through more challenges. Cheers!
-        </p>
-      </Container>
+      <TextContainer>
+        <div>
+          <h1>Frontend Mentor Solutions</h1>
+          <p>
+            Links are provided for each project's respective Github repo and
+            Live site. This is a work in progress, so expect to see more changes
+            as I progress through more challenges. Cheers!
+          </p>
+        </div>
+        <Filter>
+          <p className="header">Show projects built with:</p>
+          <div>
+            <CheckBox boxName="React" inputId="react" />
+            <CheckBox boxName="CSS Grid" inputId="cssgrid" />
+          </div>
+        </Filter>
+      </TextContainer>
     </Text>
   );
 };
