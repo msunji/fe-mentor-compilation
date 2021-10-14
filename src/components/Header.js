5440752454d8b41c5eada98dacd6b429819726f4
Marge <marge.consunji@gmail.com>
2021-10-14 14:35:33 +0800

    Remobed grid, about this site blurb

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,38 +3,30 @@ import { Container } from "./layout/Container";
 
 const Text = styled.section`
   padding: calc(2.5 * var(--padding-y)) 0;
-  background-color: var(--dark-grey);
   color: white;
+  position: relative;
+
+  &::before {
+    content: "";
+    width: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    height: 10px;
+    background-image: linear-gradient(
+      64.5deg,
+      rgba(245, 116, 185, 1) 14.7%,
+      rgba(89, 97, 223, 1) 88.7%
+    );
+  }
 `;
 
 const TextContainer = styled(Container)`
-  display: grid;
-  grid-template-columns: 1fr;
-
   h1 {
     padding-top: 0;
-  }
-
-  .about-build {
-    color: grey;
-    margin-top: 3rem;
-
-    .header {
-      text-transform: uppercase;
-      margin-bottom: 0;
-    }
-  }
-
-  @media screen and (min-width: 740px) {
-    grid-template-columns: 1fr 1fr;
-
-    .about-build {
-      margin-left: 4rem;
-      margin-top: 0;
-    }
-  }
-  @media screen and (min-width: 1100px) {
-    grid-template-columns: 1.5fr 1fr;
+    font-weight: 700;
+    color: var(--periwinkle);
   }
 `;
 
@@ -42,21 +34,12 @@ export const Header = () => {
   return (
     <Text>
       <TextContainer>
-        <div>
-          <h1>Frontend Mentor Solutions</h1>
-          <p>
-            Links are provided for each project's respective Github repo and
-            Live site. This is a work in progress, so expect to see more changes
-            as I progress through more challenges. Cheers!
-          </p>
-        </div>
-        <div className="about-build">
-          <p className="header">about these projects</p>
-          <p>
-            For the most part, I built these projects with React or Next.js and
-            styled-components.
-          </p>
-        </div>
+        <h1>Frontend Mentor Solutions</h1>
+        <p>
+          Links are provided for each project's respective Github repo and live
+          site. This is a work in progress, so expect to see more changes as I
+          progress through more challenges. Cheers!
+        </p>
       </TextContainer>
     </Text>
   );
