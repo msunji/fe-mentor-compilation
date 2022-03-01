b900c333c216d8fcb2ebd5a681d43d01f790ba30
Marge <marge.consunji@gmail.com>
2022-03-01 14:38:46 +0800

    Change styling
    Remove custom Styled Components components, and replace with SCSS

--- client/src/components/solution-components/Solutions.js
+++ client/src/components/solution-components/Solutions.js
@@ -1,47 +1,17 @@
-import axios from "axios";
+import "../../styles/components/solutions.scss";
 import { useEffect, useState } from "react";
-import styled from "styled-components";
-import { SkeletonCard } from "../layout/SkeletonCard";
-import { Solution } from "./Solution";
+import Solution from "./Solution";
+import SkeletonCard from "./SkeletonCard";
 
-const SolutionsSection = styled.section`
-  padding: 2rem 0;
-
-  @media screen and (min-width: 1000px) {
-    max-width: 600px;
-    position: absolute;
-    right: 0;
-  }
-  @media screen and (min-width: 1200px) {
-    max-width: 750px;
-    position: absolute;
-    right: 0;
-  }
-`;
-
-const SolutionsContainer = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-gap: 20px;
-
-  @media screen and (min-width: 700px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-  @media screen and (min-width: 1200px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-`;
-
-export const Solutions = () => {
+const Solutions = () => {
   const [solutions, setSolutions] = useState([]);
-
   const loadSolutions = async () => {
     try {
-      const response = await axios.get("/api/solutions");
-      setSolutions(response.data.solutions);
+      const response = await fetch("/api/solutions");
+      const data = await response.json();
+      setSolutions(data.solutions);
     } catch (err) {
-      console.error(err);
+      console.err(err);
     }
   };
 
@@ -50,14 +20,16 @@ export const Solutions = () => {
   }, []);
 
   return (
-    <SolutionsSection>
-      <SolutionsContainer>
+    <section className="solutions">
+      <div className="solutions-container">
         {solutions.length
           ? solutions.map((solution) => (
               <Solution key={solution.id} data={solution} />
             ))
           : [1, 2, 3, 4].map((_, i) => <SkeletonCard key={i} />)}
-      </SolutionsContainer>
-    </SolutionsSection>
+      </div>
+    </section>
   );
 };
+
+export default Solutions;
