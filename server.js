3d28fcdef3c9678e7fd9ed69d9aca05152c9e5ac
Marge <marge.consunji@gmail.com>
2022-03-08 15:42:12 +0800

    Remove unused redis import

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
 const PORT = 3001;
 
-const { getSolutions } = require('./db-connection');
+const { getSolutions } = require("./db-connection");
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, './client/build')));
-};
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
+}
 
-app.get('/api/solutions', async (req, res) => {
+app.get("/api/solutions", async (req, res) => {
   try {
     const solutionData = await getSolutions();
     res.status(200).send(solutionData);
   } catch (err) {
     res.status(400).send(err);
   }
-
 });
 
-app.listen(process.env.PORT || PORT, () => { console.log(`App listening at Port ${PORT}`)});
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`App listening at Port ${PORT}`);
+});
